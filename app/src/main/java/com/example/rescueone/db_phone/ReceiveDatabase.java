f36a1fe26d968f9fcf093ec1b5e729aacc78125e
Haeruemii <ehl3288@naver.com>
2020-03-07 01:35:00 +0900

    setting

--- app/src/main/java/com/example/rescueone/db_phone/ReceiveDatabase.java
+++ app/src/main/java/com/example/rescueone/db_phone/ReceiveDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {ReceiveData.class},version = 1)
+@Database(entities = {ReceiveData.class},version = 1,exportSchema = false)
 public abstract class ReceiveDatabase extends RoomDatabase {
     public abstract ReceiveDataDao receiveDataDao();
 
