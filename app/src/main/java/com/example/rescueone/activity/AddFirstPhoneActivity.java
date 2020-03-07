46f03d360f88ad15ca00be7ef4c535a696e3b4ab
Haeruemii <ehl3288@naver.com>
2020-03-07 21:09:04 +0900

    permission까지

--- app/src/main/java/com/example/rescueone/activity/AddFirstPhoneActivity.java
+++ app/src/main/java/com/example/rescueone/activity/AddFirstPhoneActivity.java
@@ -1,16 +1,74 @@
 package com.example.rescueone.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.rescueone.R;
+import com.example.rescueone.databinding.ActivityAddFirstPhoneBinding;
+import com.example.rescueone.db_server.EmergencyContact;
+import com.example.rescueone.db_server.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class AddFirstPhoneActivity extends AppCompatActivity {
+
+    EditText name;
+    EditText phone;
+    ImageButton check;
+    Toolbar toolbar;
+
+    ActivityAddFirstPhoneBinding mBinding;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    String uid = currentUser.getUid();
+    User user;
+    ArrayList<EmergencyContact> datas = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_first_phone);
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_add_first_phone);
+
+        name = findViewById(R.id.name);
+        phone = findViewById(R.id.phone);
+        check = findViewById(R.id.addFirst);
+        toolbar = findViewById(R.id.toolBar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        check.setOnClickListener(view -> {
+            new EmergencyContact(name.getText().toString(),
+                    phone.getText().toString());
+            Log.e("first add",uid+" "+phone.getText().toString());
+            mDatabase.child("users").child(uid).child("emergencyContact").child(phone.getText().toString()).setValue(name.getText().toString());
+
+            Intent intent = new Intent(getApplication(), LoginActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            finish();
+        });
+    }
+    public void OnClick(View view){
+        if(view.equals(R.id.root)){
+            InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(mBinding.name.getWindowToken(),0);
+        }
     }
 }
